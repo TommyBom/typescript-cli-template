221c336840b6ef6f8790ed5ac7a468edd8a98dc2
Maxim Uhlemann <maxim.uhlememann@lynq.tech>
2024-03-28 15:06:23 +0100

    Fix format

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,17 @@
-
 import { program } from 'commander';
 
-program
-    .argument('example')
-    .option('-v, --verbose', 'Print verbose output');
+program.argument('example').option('-v, --verbose', 'Print verbose output');
 
 program.parse();
 
 (async () => {
-    const example = program.args[0];
-    const options = program.opts();
-    const verbose = options['verbose'];
+  const example = program.args[0];
+  const options = program.opts();
+  const verbose = options['verbose'];
 
-    if (verbose) {
-        console.log('Well hello there world', example);
-    } else {
-        console.log('Hello world', example);
-    }
+  if (verbose) {
+    console.log('Well hello there world', example);
+  } else {
+    console.log('Hello world', example);
+  }
 })();
