25288fec00efb3441e4e7c0e239d9856ddc81abf
Maxim Uhlemann <maxim.uhlememann@lynq.tech>
2024-03-28 15:05:19 +0100

    Add example cli

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+
+import { program } from 'commander';
+
+program
+    .argument('example')
+    .option('-v, --verbose', 'Print verbose output');
+
+program.parse();
+
+(async () => {
+    const example = program.args[0];
+    const options = program.opts();
+    const verbose = options['verbose'];
+
+    if (verbose) {
+        console.log('Well hello there world', example);
+    } else {
+        console.log('Hello world', example);
+    }
+})();
